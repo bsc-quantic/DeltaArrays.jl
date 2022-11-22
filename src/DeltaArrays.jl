02852b8a59e58158dee5fd24d9ee71a512cc2ffd
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-22 17:02:46 +0100

    Refactor fix for namespace clash with `Array`

--- src/DeltaArrays.jl
+++ src/DeltaArrays.jl
@@ -3,7 +3,7 @@ module DeltaArrays
 using LinearAlgebra
 import Base: similar, copyto!, ndims, size, getindex, parent, real, imag
 import Base: -, +, ==
-import LinearAlgebra: diagzero, ishermitian, issymmetric, isposdef, factorize
+import LinearAlgebra: diagzero, ishermitian, issymmetric, isposdef, factorize, Array
 
 export DeltaArray, delta, deltaind
 
@@ -134,7 +134,7 @@ DeltaArray{T,N}(D::DeltaArray) where {T,N} = DeltaArray{T,N}(D.data)
 AbstractArray{T}(D::DeltaArray) where {T} = DeltaArray{T}(D)
 AbstractArray{T,N}(D::DeltaArray) where {T,N} = DeltaArray{T,N}(D)
 Array(D::DeltaArray{T,N}) where {T,N} = Array{promote_type(T, typeof(zero(T)))}(D)
-function Base.Array{T,N}(D::DeltaArray) where {T,N}
+function Array{T,N}(D::DeltaArray) where {T,N}
     n = size(D, 1)
     B = zeros(T, ntuple(_ -> n, N))
     @inbounds for i in 1:n
