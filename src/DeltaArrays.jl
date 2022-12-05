6e668429bca4d41d291b7ede1d6cf9613d797fd1
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-12-05 14:27:00 +0100

    Add missing `*`,`/` methods

--- src/DeltaArrays.jl
+++ src/DeltaArrays.jl
@@ -1,10 +1,10 @@
 module DeltaArrays
 
-using LinearAlgebra: LinearAlgebra, sym_uplo, AdjointAbsVec, TransposeAbsVec, AbstractTriangular
+using LinearAlgebra: LinearAlgebra, sym_uplo, AdjointAbsVec, TransposeAbsVec, AbstractTriangular, AbstractVecOrMat, HermOrSym, QRCompactWYQ, QRPackedQ
 import Core: Array
 import Base: similar, copyto!, size, getindex, setindex!, parent, real, imag, iszero, isone, conj, conj!, adjoint, transpose, permutedims, inv, sum, kron, kron!
 import Base: -, +, ==, *, /, \, ^
-import LinearAlgebra: ishermitian, issymmetric, isposdef, factorize, isdiag, diag, tr, det, logdet, logabsdet, pinv, eigvals, eigvecs, eigen, svdvals, svd, istriu, istril, triu!, tril!
+import LinearAlgebra: ishermitian, issymmetric, isposdef, factorize, isdiag, diag, tr, det, logdet, logabsdet, pinv, eigvals, eigvecs, eigen, svdvals, svd, istriu, istril, triu!, tril!, lmul!, rmul!, ldiv!, rdiv!
 
 export DeltaArray, delta, deltaind
 
@@ -287,6 +287,18 @@ function checkmulsize(A, B)
     return nothing
 end
 
+checksizeout(C, ::DeltaArray{<:Any,2}, A) = checksizeout(C, A)
+checksizeout(C, A, ::DeltaArray{<:Any,2}) = checksizeout(C, A)
+checksizeout(C, A::DeltaArray{<:Any,2}, ::DeltaArray{<:Any,2}) = checksizeout(C, A)
+function checksizeout(C, A)
+    szA = size(A)
+    szC = size(C)
+    szA == szC || throw(DimensionMismatch("output matrix has size: $szC, but should have size $szA"))
+    return nothing
+end
+
+checkmulsize(C, A, B) = (checkmulsize(A, B); checksizeout(C, A, B))
+
 function (*)(Da::DeltaArray{<:Any,2}, Db::DeltaArray{<:Any,2})
     checkmulsize(Da, Db)
     return DeltaArray{2}(Da.data .* Db.data)
@@ -297,7 +309,310 @@ function (*)(Da::DeltaArray{<:Any,2}, V::AbstractVector)
     return D.data .* V
 end
 
-# TODO ...
+(*)(A::AbstractMatrix, D::DeltaArray) = mul!(similar(A, Base.promote_op(*, eltype(A), eltype(D)), size(A)), A, D)
+(*)(D::DeltaArray, A::AbstractMatrix) = mul!(similar(A, promote_op(*, eltype(A), eltype(D)), size(A)), D, A)
+
+rmul!(A::AbstractMatrix, D::DeltaArray{<:Any,2}) = @inline mul!(A, A, D)
+lmul!(D::DeltaArray{<:Any,2}, B::AbstractVecOrMat) = @inline mul!(B, D, B)
+
+function *(A::AdjOrTransAbsMat, D::DeltaArray{<:Any,2})
+    Ac = LinearAlgebra.copy_similar(A, Base.promote_op(*, eltype(A), eltype(D.data)))
+    rmul!(Ac, D)
+end
+
+*(D::DeltaArray{<:Any,2}, adjQ::Adjoint{<:Any,<:Union{QRCompactWYQ,QRPackedQ}}) =
+    rmul!(Array{promote_type(eltype(D), eltype(adjQ))}(D), adjQ)
+
+function *(D::DeltaArray{<:Any,2}, A::AdjOrTransAbsMat)
+    Ac = LinearAlgebra.copy_similar(A, Base.promote_op(*, eltype(A), eltype(D.data)))
+    lmul!(D, Ac)
+end
+
+@inline function __muldiag!(out, D::DeltaArray{<:Any,2}, B, alpha, beta)
+    Base.require_one_based_indexing(B)
+    Base.require_one_based_indexing(out)
+    if iszero(alpha)
+        LinearAlgebra._rmul_or_fill!(out, beta)
+    else
+        if iszero(beta)
+            @inbounds for j in axes(B, 2)
+                @simd for i in axes(B, 1)
+                    out[i, j] = D.data[i] * B[i, j] * alpha
+                end
+            end
+        else
+            @inbounds for j in axes(B, 2)
+                @simd for i in axes(B, 1)
+                    out[i, j] = D.data[i] * B[i, j] * alpha + out[i, j] * beta
+                end
+            end
+        end
+    end
+    return out
+end
+@inline function __muldiag!(out, A, D::DeltaArray{<:Any,2}, alpha, beta)
+    Base.require_one_based_indexing(A)
+    Base.require_one_based_indexing(out)
+    if iszero(alpha)
+        LinearAlgebra._rmul_or_fill!(out, beta)
+    else
+        if iszero(beta)
+            @inbounds for j in axes(A, 2)
+                dja = D.data[j] * alpha
+                @simd for i in axes(A, 1)
+                    out[i, j] = A[i, j] * dja
+                end
+            end
+        else
+            @inbounds for j in axes(A, 2)
+                dja = D.data[j] * alpha
+                @simd for i in axes(A, 1)
+                    out[i, j] = A[i, j] * dja + out[i, j] * beta
+                end
+            end
+        end
+    end
+    return out
+end
+@inline function __muldiag!(out::DeltaArray{<:Any,2}, D1::DeltaArray{<:Any,2}, D2::DeltaArray{<:Any,2}, alpha, beta)
+    d1 = D1.data
+    d2 = D2.data
+    if iszero(alpha)
+        LinearAlgebra._rmul_or_fill!(out.data, beta)
+    else
+        if iszero(beta)
+            @inbounds @simd for i in eachindex(out.data)
+                out.data[i] = d1[i] * d2[i] * alpha
+            end
+        else
+            @inbounds @simd for i in eachindex(out.data)
+                out.data[i] = d1[i] * d2[i] * alpha + out.data[i] * beta
+            end
+        end
+    end
+    return out
+end
+@inline function __muldiag!(out, D1::DeltaArray{<:Any,2}, D2::DeltaArray{<:Any,2}, alpha, beta)
+    Base.require_one_based_indexing(out)
+    mA = size(D1, 1)
+    d1 = D1.data
+    d2 = D2.data
+    LinearAlgebra._rmul_or_fill!(out, beta)
+    if !iszero(alpha)
+        @inbounds @simd for i in 1:mA
+            out[i, i] += d1[i] * d2[i] * alpha
+        end
+    end
+    return out
+end
+
+@inline function _muldiag!(out, A, B, alpha, beta)
+    checksizeout(out, A, B)
+    __muldiag!(out, A, B, alpha, beta)
+    return out
+end
+
+function (*)(Da::DeltaArray{<:Any,2}, A::AbstractMatrix, Db::DeltaArray{<:Any,2})
+    return broadcast(*, Da.data, A, permutedims(Db.data))
+end
+
+# Get ambiguous method if try to unify AbstractVector/AbstractMatrix here using AbstractVecOrMat
+@inline mul!(out::AbstractVector, D::DeltaArray{<:Any,2}, V::AbstractVector, alpha::Number, beta::Number) = _muldiag!(out, D, V, alpha, beta)
+@inline mul!(out::AbstractMatrix, D::DeltaArray{<:Any,2}, B::AbstractMatrix, alpha::Number, beta::Number) = _muldiag!(out, D, B, alpha, beta)
+@inline mul!(out::AbstractMatrix, D::DeltaArray{<:Any,2}, B::Adjoint{<:Any,<:AbstractVecOrMat}, alpha::Number, beta::Number) = _muldiag!(out, D, B, alpha, beta)
+@inline mul!(out::AbstractMatrix, D::DeltaArray{<:Any,2}, B::Transpose{<:Any,<:AbstractVecOrMat}, alpha::Number, beta::Number) = _muldiag!(out, D, B, alpha, beta)
+
+@inline mul!(out::AbstractMatrix, A::AbstractMatrix, D::DeltaArray{<:Any,2}, alpha::Number, beta::Number) = _muldiag!(out, A, D, alpha, beta)
+@inline mul!(out::AbstractMatrix, A::Adjoint{<:Any,<:AbstractVecOrMat}, D::DeltaArray{<:Any,2}, alpha::Number, beta::Number) = _muldiag!(out, A, D, alpha, beta)
+@inline mul!(out::AbstractMatrix, A::Transpose{<:Any,<:AbstractVecOrMat}, D::DeltaArray{<:Any,2}, alpha::Number, beta::Number) = _muldiag!(out, A, D, alpha, beta)
+@inline mul!(C::DeltaArray{<:Any,2}, Da::DeltaArray{<:Any,2}, Db::DeltaArray{<:Any,2}, alpha::Number, beta::Number) = _muldiag!(C, Da, Db, alpha, beta)
+
+mul!(C::AbstractMatrix, Da::DeltaArray{<:Any,2}, Db::DeltaArray{<:Any,2}, alpha::Number, beta::Number) = _muldiag!(C, Da, Db, alpha, beta)
+
+/(A::AbstractVecOrMat, D::DeltaArray{<:Any,2}) = _rdiv!(similar(A, LinearAlgebra._init_eltype(/, eltype(A), eltype(D))), A, D)
+/(A::HermOrSym, D::DeltaArray{<:Any,2}) = _rdiv!(similar(A, LinearAlgebra._init_eltype(/, eltype(A), eltype(D)), size(A)), A, D)
+rdiv!(A::AbstractVecOrMat, D::DeltaArray{<:Any,2}) = @inline _rdiv!(A, A, D)
+
+# avoid copy when possible via internal 3-arg backend
+function _rdiv!(B::AbstractVecOrMat, A::AbstractVecOrMat, D::DeltaArray{<:Any,2})
+    require_one_based_indexing(A)
+    dd = D.data
+    m, n = size(A, 1), size(A, 2)
+    if (k = length(dd)) != n
+        throw(DimensionMismatch("left hand side has $n columns but D is $k by $k"))
+    end
+    @inbounds for j in 1:n
+        ddj = dd[j]
+        iszero(ddj) && throw(SingularException(j))
+        for i in 1:m
+            B[i, j] = A[i, j] / ddj
+        end
+    end
+    B
+end
+
+function (\)(D::DeltaArray{<:Any,2}, B::AbstractVector)
+    j = findfirst(iszero, D.data)
+    isnothing(j) || throw(SingularException(j))
+    return D.data .\ B
+end
+
+\(D::DeltaArray{<:Any,2}, B::AbstractMatrix) = ldiv!(similar(B, LinearAlgebra._init_eltype(\, eltype(D), eltype(B))), D, B)
+\(D::DeltaArray{<:Any,2}, B::HermOrSym) = ldiv!(similar(B, LinearAlgebra._init_eltype(\, eltype(D), eltype(B)), size(B)), D, B)
+
+ldiv!(D::DeltaArray{<:Any,2}, B::AbstractVecOrMat) = @inline ldiv!(B, D, B)
+function ldiv!(B::AbstractVecOrMat, D::DeltaArray{<:Any,2}, A::AbstractVecOrMat)
+    require_one_based_indexing(A, B)
+    dd = D.data
+    d = length(dd)
+    m, n = size(A, 1), size(A, 2)
+    m′, n′ = size(B, 1), size(B, 2)
+    m == d || throw(DimensionMismatch("right hand side has $m rows but D is $d by $d"))
+    (m, n) == (m′, n′) || throw(DimensionMismatch("expect output to be $m by $n, but got $m′ by $n′"))
+    j = findfirst(iszero, D.data)
+    isnothing(j) || throw(SingularException(j))
+    @inbounds for j = 1:n, i = 1:m
+        B[i, j] = dd[i] \ A[i, j]
+    end
+    B
+end
+
+# Optimizations for \, / between DeltaArrays
+\(D::DeltaArray{<:Any,2}, B::DeltaArray{<:Any,2}) = ldiv!(similar(B, Base.promote_op(\, eltype(D), eltype(B))), D, B)
+/(A::DeltaArray{<:Any,2}, D::DeltaArray{<:Any,2}) = _rdiv!(similar(A, Base.promote_op(/, eltype(A), eltype(D))), A, D)
+function _rdiv!(Dc::DeltaArray{<:Any,2}, Db::DeltaArray{<:Any,2}, Da::DeltaArray{<:Any,2})
+    n, k = length(Db.data), length(Da.data)
+    n == k || throw(DimensionMismatch("left hand side has $n columns but D is $k by $k"))
+    j = findfirst(iszero, Da.data)
+    isnothing(j) || throw(SingularException(j))
+    Dc.data .= Db.data ./ Da.data
+    Dc
+end
+ldiv!(Dc::DeltaArray{<:Any,2}, Da::DeltaArray{<:Any,2}, Db::DeltaArray{<:Any,2}) = DeltaArray{2}(ldiv!(Dc.data, Da, Db.data))
+
+# optimizations for (Sym)Tridiagonal and DeltaArray
+@propagate_inbounds _getudiag(T::Tridiagonal, i) = T.du[i]
+@propagate_inbounds _getudiag(S::SymTridiagonal, i) = S.ev[i]
+@propagate_inbounds _getdiag(T::Tridiagonal, i) = T.d[i]
+@propagate_inbounds _getdiag(S::SymTridiagonal, i) = symmetric(S.dv[i], :U)::LinearAlgebra.symmetric_type(eltype(S.dv))
+@propagate_inbounds _getldiag(T::Tridiagonal, i) = T.dl[i]
+@propagate_inbounds _getldiag(S::SymTridiagonal, i) = transpose(S.ev[i])
+
+function (\)(D::DeltaArray{<:Any,2}, S::SymTridiagonal)
+    T = Base.promote_op(\, eltype(D), eltype(S))
+    du = similar(S.ev, T, max(length(S.dv) - 1, 0))
+    d = similar(S.dv, T, length(S.dv))
+    dl = similar(S.ev, T, max(length(S.dv) - 1, 0))
+    ldiv!(Tridiagonal(dl, d, du), D, S)
+end
+(\)(D::DeltaArray{<:Any,2}, T::Tridiagonal) = ldiv!(similar(T, Base.promote_op(\, eltype(D), eltype(T))), D, T)
+function ldiv!(T::Tridiagonal, D::DeltaArray{<:Any,2}, S::Union{SymTridiagonal,Tridiagonal})
+    m = size(S, 1)
+    dd = D.data
+    if (k = length(dd)) != m
+        throw(DimensionMismatch("diagonal matrix is $k by $k but right hand side has $m rows"))
+    end
+    if length(T.d) != m
+        throw(DimensionMismatch("target matrix size $(size(T)) does not match input matrix size $(size(S))"))
+    end
+    m == 0 && return T
+    j = findfirst(iszero, dd)
+    isnothing(j) || throw(SingularException(j))
+    ddj = dd[1]
+    T.d[1] = ddj \ _getdiag(S, 1)
+    @inbounds if m > 1
+        T.du[1] = ddj \ _getudiag(S, 1)
+        for j in 2:m-1
+            ddj = dd[j]
+            T.dl[j-1] = ddj \ _getldiag(S, j - 1)
+            T.d[j] = ddj \ _getdiag(S, j)
+            T.du[j] = ddj \ _getudiag(S, j)
+        end
+        ddj = dd[m]
+        T.dl[m-1] = ddj \ _getldiag(S, m - 1)
+        T.d[m] = ddj \ _getdiag(S, m)
+    end
+    return T
+end
+
+function (/)(S::SymTridiagonal, D::DeltaArray{<:Any,2})
+    T = Base.promote_op(\, eltype(D), eltype(S))
+    du = similar(S.ev, T, max(length(S.dv) - 1, 0))
+    d = similar(S.dv, T, length(S.dv))
+    dl = similar(S.ev, T, max(length(S.dv) - 1, 0))
+    _rdiv!(Tridiagonal(dl, d, du), S, D)
+end
+(/)(T::Tridiagonal, D::DeltaArray{<:Any,2}) = _rdiv!(similar(T, Base.promote_op(/, eltype(T), eltype(D))), T, D)
+function _rdiv!(T::Tridiagonal, S::Union{SymTridiagonal,Tridiagonal}, D::DeltaArray{<:Any,2})
+    n = size(S, 2)
+    dd = D.data
+    if (k = length(dd)) != n
+        throw(DimensionMismatch("left hand side has $n columns but D is $k by $k"))
+    end
+    if length(T.d) != n
+        throw(DimensionMismatch("target matrix size $(size(T)) does not match input matrix size $(size(S))"))
+    end
+    n == 0 && return T
+    j = findfirst(iszero, dd)
+    isnothing(j) || throw(SingularException(j))
+    ddj = dd[1]
+    T.d[1] = _getdiag(S, 1) / ddj
+    @inbounds if n > 1
+        T.dl[1] = _getldiag(S, 1) / ddj
+        for j in 2:n-1
+            ddj = dd[j]
+            T.dl[j] = _getldiag(S, j) / ddj
+            T.d[j] = _getdiag(S, j) / ddj
+            T.du[j-1] = _getudiag(S, j - 1) / ddj
+        end
+        ddj = dd[n]
+        T.d[n] = _getdiag(S, n) / ddj
+        T.du[n-1] = _getudiag(S, n - 1) / ddj
+    end
+    return T
+end
+
+# Optimizations for [l/r]mul!, l/rdiv!, *, / and \ between Triangular and DeltaArray.
+# These functions are generally more efficient if we calculate the whole data field.
+# The following code implements them in a unified pattern to avoid missing.
+@inline function _setdiag!(data, f, diag, diag′=nothing)
+    @inbounds for i in 1:length(diag)
+        data[i, i] = isnothing(diag′) ? f(diag[i]) : f(diag[i], diag′[i])
+    end
+    data
+end
+for Tri in (:UpperTriangular, :LowerTriangular)
+    UTri = Symbol(:Unit, Tri)
+    # 2 args
+    for (fun, f) in zip((:*, :rmul!, :rdiv!, :/), (:identity, :identity, :inv, :inv))
+        @eval $fun(A::$Tri, D::DeltaArray{<:Any,2}) = $Tri($fun(A.data, D))
+        @eval $fun(A::$UTri, D::DeltaArray{<:Any,2}) = $Tri(_setdiag!($fun(A.data, D), $f, D.data))
+    end
+    for (fun, f) in zip((:*, :lmul!, :ldiv!, :\), (:identity, :identity, :inv, :inv))
+        @eval $fun(D::DeltaArray{<:Any,2}, A::$Tri) = $Tri($fun(D, A.data))
+        @eval $fun(D::DeltaArray{<:Any,2}, A::$UTri) = $Tri(_setdiag!($fun(D, A.data), $f, D.data))
+    end
+    # 3-arg ldiv!
+    @eval ldiv!(C::$Tri, D::DeltaArray{<:Any,2}, A::$Tri) = $Tri(ldiv!(C.data, D, A.data))
+    @eval ldiv!(C::$Tri, D::DeltaArray{<:Any,2}, A::$UTri) = $Tri(_setdiag!(ldiv!(C.data, D, A.data), inv, D.data))
+    # 3-arg mul!: invoke 5-arg mul! rather than lmul!
+    @eval mul!(C::$Tri, A::Union{$Tri,$UTri}, D::DeltaArray{<:Any,2}) = mul!(C, A, D, true, false)
+    # 5-arg mul!
+    @eval @inline mul!(C::$Tri, D::DeltaArray{<:Any,2}, A::$Tri, α::Number, β::Number) = $Tri(mul!(C.data, D, A.data, α, β))
+    @eval @inline function mul!(C::$Tri, D::DeltaArray{<:Any,2}, A::$UTri, α::Number, β::Number)
+        iszero(α) && return LinearAlgebra._rmul_or_fill!(C, β)
+        diag′ = iszero(β) ? nothing : diag(C)
+        data = mul!(C.data, D, A.data, α, β)
+        $Tri(_setdiag!(data, MulAddMul(α, β), D.data, diag′))
+    end
+    @eval @inline mul!(C::$Tri, A::$Tri, D::DeltaArray{<:Any,2}, α::Number, β::Number) = $Tri(mul!(C.data, A.data, D, α, β))
+    @eval @inline function mul!(C::$Tri, A::$UTri, D::DeltaArray{<:Any,2}, α::Number, β::Number)
+        iszero(α) && return LinearAlgebra._rmul_or_fill!(C, β)
+        diag′ = iszero(β) ? nothing : diag(C)
+        data = mul!(C.data, A.data, D, α, β)
+        $Tri(_setdiag!(data, MulAddMul(α, β), D.data, diag′))
+    end
+end
 
 kron(A::DeltaArray{<:Any,N}, B::DeltaArray{<:Any,M}) where {N,M} = DeltaArray{N + M}(kron(A.data, B.data))
 
